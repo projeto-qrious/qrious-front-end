cee64bff6b46af024b2c5e338196fd70af192af8
Gabrielk127 <gabrielfsilva09@gmail.com>
2024-10-10 14:40:09 -0300

    add question page

--- src/app/components/Card.tsx
+++ src/app/components/Card.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import React, { useState } from "react";
 import { FaRegHeart, FaHeart } from "react-icons/fa";
 
@@ -17,22 +18,24 @@ const Card: React.FC<CardProps> = ({ title, text, initialLikes }) => {
   };
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg p-4 bg-white my-6">
-      <div className="flex items-start mb-4">
-        <h2 className="text-xl font-bold">{title}</h2>
+    <Link href={{ pathname: "/question", query: { title, text } }}>
+      <div className="max-w-sm rounded overflow-hidden shadow-lg p-4 bg-white my-6">
+        <div className="flex items-start mb-4">
+          <h2 className="text-xl font-bold">{title}</h2>
+        </div>
+        <p className="text-gray-700 text-base mb-4 line-clamp-3">{text}</p>
+        <div className="flex items-center">
+          <button onClick={handleLike} className="flex items-center">
+            {isLiked ? (
+              <FaHeart className="text-red-500 mr-1 h-5 w-5" />
+            ) : (
+              <FaRegHeart className="text-red-500 mr-1 h-5 w-5" />
+            )}
+            <span className="text-gray-600">{likes}</span>
+          </button>
+        </div>
       </div>
-      <p className="text-gray-700 text-base mb-4 line-clamp-3">{text}</p>
-      <div className="flex items-center">
-        <button onClick={handleLike} className="flex items-center">
-          {isLiked ? (
-            <FaHeart className="text-red-500 mr-1 h-5 w-5" />
-          ) : (
-            <FaRegHeart className="text-gray-500 mr-1 h-5 w-5" />
-          )}
-          <span className="text-gray-600">{likes}</span>
-        </button>
-      </div>
-    </div>
+    </Link>
   );
 };
 
