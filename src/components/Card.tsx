2fb9d537250141b3ee2b19b0c73c945a122fe066
Adrian Campana <adrianphcampana12@gmail.com>
2024-10-10 14:38:02 -0300

    :sparkles: feat: add animation to cards

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { FaRegHeart, FaHeart } from "react-icons/fa";
+import { motion } from "framer-motion";
 
 interface CardProps {
   title: string;
@@ -17,12 +18,15 @@ const Card: React.FC<CardProps> = ({ title, text, initialLikes }) => {
   };
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg p-4 bg-white my-6">
+    <motion.div
+      whileHover={{ scale: 1.05 }}
+      className="max-w-sm rounded overflow-hidden shadow-lg p-4 bg-white my-6 cursor-pointer"
+    >
       <div className="flex items-start mb-4">
         <h2 className="text-xl font-bold">{title}</h2>
       </div>
       <p className="text-gray-700 text-base mb-4 line-clamp-3">{text}</p>
-      <div className="flex items-center">
+      <motion.div whileTap={{ scale: 1.02 }} className="flex items-center">
         <button onClick={handleLike} className="flex items-center">
           {isLiked ? (
             <FaHeart className="text-red-500 mr-1 h-5 w-5" />
@@ -31,8 +35,8 @@ const Card: React.FC<CardProps> = ({ title, text, initialLikes }) => {
           )}
           <span className="text-gray-600">{likes}</span>
         </button>
-      </div>
-    </div>
+      </motion.div>
+    </motion.div>
   );
 };
 
